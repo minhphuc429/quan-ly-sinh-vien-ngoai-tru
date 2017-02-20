36dfeaa8a0eb49ef63a2592e45ad51ceda59d3e9
Phuc Nguyen Minh <minhphuc429@gmail.com>
2017-02-20 14:17:40 +0700

    update .gitignore

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -2,10 +2,10 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>
-        Dashboard
-        <small>Control panel</small>
-    </h1>
+        <h1>
+            Dashboard
+            <small>@yield('content-header')</small>
+        </h1>
     <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Dashboard</li>
