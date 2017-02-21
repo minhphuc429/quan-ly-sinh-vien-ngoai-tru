c827f919f77713e62fc30cafbb42f6563baac9b8
Phuc Nguyen Minh <minhphuc429@gmail.com>
2017-02-21 08:29:45 +0700

    cập nhật view ngoại trú

--- resources/views/ngoaitrus/index.blade.php
+++ resources/views/ngoaitrus/index.blade.php
@@ -1,45 +1,61 @@
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
+@extends('layouts.app')
+
+@section('title', 'Thông Tin Ngoại Trú')
+
+@section('content-header', 'Ngoại Trú')
+
+@section('content')
+
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul class="fa-ul">
+                @foreach ($errors->all() as $error)
+                    <li class="fa-li fa fa-chevron-circle-right">{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="row" style="margin-bottom: 20px; ">
+        <div class="col-sm-2">
+            <a class="btn btn-info" href="{{ action('NgoaiTruController@create') }}">Thêm thông tin ngoại trú</a>
+        </div>
     </div>
-@endif
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul class="fa-ul">
-            @foreach ($errors->all() as $error)
-                <li class="fa-li fa fa-chevron-circle-right">{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<a href="{{ route('ngoaitrus.create') }}">Create</a>
-<div class="table-responsive">
-    <table class="table table-hover">
-        <thead>
-        <tr>
-            <th>STT</th>
-            <th>Mã Lớp</th>
-            <th>Tên Lớp</th>
-            <th>Mã Khoa</th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        @php
-            $i = 0;
-        @endphp
-        @foreach ($ngoaitrus->all() as $ngoaitru)
+
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
             <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $ngoaitru->Mangoaitru }}</td>
-                <td>{{ $ngoaitru->Tenngoaitru }}</td>
-                <td>{{ $ngoaitru->MaKhoa }}</td>
-                <td>
-                    <a href="{{ route('ngoaitrus.show', $ngoaitru->id) }}" class="btn btn-info">View Task</a>
-                    <a href="{{ route('ngoaitrus.edit', $ngoaitru->id) }}" class="btn btn-primary">Edit Task</a>
-                </td>
+                <th>STT</th>
+                <th>Mã Lớp</th>
+                <th>Tên Lớp</th>
+                <th>Mã Khoa</th>
+                <th></th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+            @php
+                $i = 0;
+            @endphp
+            @foreach ($ngoaitrus->all() as $ngoaitru)
+                <tr>
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $ngoaitru->Mangoaitru }}</td>
+                    <td>{{ $ngoaitru->Tenngoaitru }}</td>
+                    <td>{{ $ngoaitru->MaKhoa }}</td>
+                    <td>
+                        <a href="{{ route('ngoaitrus.show', $ngoaitru->id) }}" class="btn btn-info">View Task</a>
+                        <a href="{{ route('ngoaitrus.edit', $ngoaitru->id) }}" class="btn btn-primary">Edit Task</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
---
+++ resources/views/ngoaitrus/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Xem Thông Tin Ngoại Trú')
+
+@section('content-header', 'Ngoại Trú')
+
+@section('content')
+
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul class="fa-ul">
+                @foreach ($errors->all() as $error)
+                    <li class="fa-li fa fa-chevron-circle-right">{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+@endsection
