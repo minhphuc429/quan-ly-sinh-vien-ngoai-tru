52cbaab6382f88f63521c263738389af81c44082
Phuc Nguyen Minh <minhphuc429@gmail.com>
2017-02-22 10:46:27 +0700

    cập nhật view khoas.edit

--- resources/views/khoas/edit.blade.php
+++ resources/views/khoas/edit.blade.php
@@ -22,33 +22,41 @@
         </div>
     @endif
 
-    <form action="{{ route('khoas.update', $khoa->id) }}" method="POST" role="form">
-        {{ method_field('PATCH') }}
-        {{ csrf_field() }}
-        <legend>Form title</legend>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Cập Nhật Thông Tin Khoa</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form class="form-horizontal" action="{{ action('KhoaController@update', $khoa->id) }}" method="POST">
+                    {{ method_field('PATCH') }}
+                    {{ csrf_field() }}
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="makhoa" class="col-sm-2 control-label">Mã Khoa</label>
 
-        <div class="form-group">
-            <label for="makhoa" class="col-md-2 control-label">Mã Khoa</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="makhoa" name="makhoa" placeholder="CNTT" value="@if(old('makhoa')){{ old('makhoa') }}@else{{ $khoa->MaKhoa }}@endif">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="tenkhoa" class="col-sm-2 control-label">Tên Khoa</label>
 
-            <div class="col-md-10">
-                <input type="text" class="form-control" id="makhoa" name="makhoa" placeholder="" value="@if(old('makhoa')){{ old('makhoa') }}@else{{ $khoa->MaKhoa }}@endif">
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="tenkhoa" name="tenkhoa" placeholder="CNTT" value="@if(old('tenkhoa')){{ old('tenkhoa') }}@else{{ $khoa->TenKhoa }}@endif">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        <a href="{{ route('khoas.index') }}" class="btn btn-default">Hủy</a>
+                        <button type="submit" class="btn btn-success pull-right">Cập Nhật</button>
+                    </div>
+                    <!-- /.box-footer -->
+                </form>
             </div>
         </div>
-
-        <div class="form-group">
-            <label for="tenkhoa" class="col-md-2 control-label">Ten Khoa</label>
-
-            <div class="col-md-10">
-                <input type="text" class="form-control" id="tenkhoa" name="tenkhoa" placeholder="" value="@if(old('tenkhoa')){{ old('tenkhoa') }}@else{{ $khoa->TenKhoa }}@endif">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-md-offset-2">
-                <a href="{{ route('khoas.index') }}" class="btn btn-default">Back</a>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-
-    </form>
+    </div>
 @endsection
